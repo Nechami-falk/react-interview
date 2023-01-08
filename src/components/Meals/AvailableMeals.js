e99e4a86915493b25e014bd9bc0987cfd39a66f3
nechami falk <falknechami@gmail.com>
2023-01-09 01:44:33 +0200

    get meals from api

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,33 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import React, {useState, useEffect} from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
+  
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+
+  const [meals,setMeals ] =useState([]);
+
+
+  useEffect(() => {
+    const fetchDataMeals = async () => {
+      try{
+        const data = await fetch('https://react-interview-api-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+        const json = await data.json();
+        console.log(json);
+        const mealsData = Object.keys(json).map(k => ({id:k, name:json[k].name, description:json[k].description, price:json[k].price}));
+        setMeals(mealsData);  
+        console.log(mealsData);
+      }
+      catch(ex){
+        console.log(ex);
+      }
+    }
+    fetchDataMeals();
+      },[]);
+
+
+  const mealsList = meals && meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
